d5f2b17748d5a67d7c6009ced24ec042b576bfeb
Lawar James Chaudhary <140185232+lawarjameschaudhary@users.noreply.github.com>
2025-02-21 13:01:28 +0545

    some changes

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{js,jsx}"], // Ensures Tailwind scans all relevant files
-  theme: {
-    extend: {},
-  },
-  plugins: [], // Empty array unless you need additional plugins
-};
